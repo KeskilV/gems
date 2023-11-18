dfe1e91b56625bee096ffa95cf2645737148faa4
KeskilV <gemlab@mail.ru>
2023-11-19 06:36:47 +0900

    price_sap_ruby 1/2 ready

--- ipynb/module_prgem/price_sap_ruby.py
+++ ipynb/module_prgem/price_sap_ruby.py
@@ -3,79 +3,127 @@ import pandas as pd
 import re
 import os
 import importlib
-from module_prgem.dictemsapruby import dcla_em_sto, dcol_em_sto, dcla_sap, dcol_sap
-
+#from module_prgem.dictgems import dcla_em_sto, dcol_em_sto, dcla_sap, dcol_sap, dgem
+import module_prgem.dictgems as mds
 #Globals
 #загрузка таблицы определения размерностей для сапфиров и рубинов
 df_sizes_sap_ruby = pd.read_excel('module_prgem/sizes_ruby_sap_sto.xlsx') 
 #загрузка прейскуранта для сапфиров и рубинов
 pr_em_sto = pd.read_excel('module_prgem/df_pr_sap_ruby_ 64_65_13-02-2020.xlsx',
                           dtype={'цвет':'str', 'чистота':'str'})
-#последний рабочий pattern_sap3_
-pattern_sap =  r"""(?ix)(?P<N>\d{,3})# штуки 
-               (?P<Gem>сапфир[A-яA-z.]{1,5})[-]+  #gem
+df_pr_sapruby = pd.read_excel('module_prgem/df_pr_sap_ruby_ 64_65_13-02-2020.xlsx',
+                          dtype={'цвет':'str', 'чистота':'str'})
+
+def size_sap_ruby(carat_1):
+    '''функция для определения размерной группы сапфира и рубина в зависимоти кабашон или фацет огранка
+       , carat_1'''   
+    return df_sizes_sap_ruby[(df_sizes_sap_ruby.low_grade<=carat_1)].sort_values('low_grade').iloc[-1,0]
+
+def make_list_sap(t):
+    #последний рабочий pattern_sap3_
+    pattern_sap =  r"""(?ix)(?P<N>\d{,3})# штуки 
+               (?P<Gem>сапфир[A-яA-z.]{0,5})[-]+  #gem
                (?P<Carat>\d\d?[.,]?\d*)[\s-]+                                 # карат
                (?P<Form>[A-яA-z.\d-]{1,10})[+]+  #[+\s-]+ 
                (?P<ColQ>[A-яA-z]*\d*[A-яA-z]*[ /]+(?:[A-яA-z]*\d*[A-яA-z]*\d*)[*]*[A-яA-z]*)"""
 
-# чисто для сапфирП и Н
-pattern_jsap = r"""(?ix)(?P<Gem>сапфир[пПнНhH][^вВbB])+#gem"""
+    # чисто для сапфирП и Н
+    pattern_jsap = r"""(?ix)(?P<Gem>сапфир[пПнНhH][^вВbB])+#gem"""
+    def vst2listsap(t):
+        '''функцая распозавания сапфиров вывод если нет то nan'''
+        res = re.findall(pattern_sap, t )
+        return res if len(res)!=0 else np.nan 
 
-def vst2listsap(t):
-    '''функцая распозавания сапфиров вывод если нет то nan'''
-    res = re.findall(pattern_sap, t )
-    return res if len(res)!=0 else np.nan 
+    def vst2listsap_notna(t):
+        '''функцая распозавания сапфиров вывод если нет то []'''
+        res = re.findall(pattern_sap, t )
+        return res #not na -> if len(res)!=0 else np.nan 
 
-def vst2listsap_notna(t):
-    '''функцая распозавания сапфиров вывод если нет то []'''
-    res = re.findall(pattern_sap, t )
-    return res #not na -> if len(res)!=0 else np.nan 
+    def error_by_lens_patts(t):
+        '''функцая обнаружения ошибок при re распозаваниb сапфиров'''
 
-def error_by_lens_patts(t):
-    '''функцая обнаружения ошибок при re распозаваниb сапфиров'''
+        len_p = len(re.findall(pattern_sap, t ))
+        len_j = len(re.findall(pattern_jsap, t ))
+        return f"bylens:error:{len_p}!={len_j}" if len_p!=len_j else f"ok:{len_p}!={len_j}"
 
-    len_p = len(re.findall(pattern_sap, t ))
-    len_j = len(re.findall(pattern_jsap, t ))
-    return f"error{len_p}!={len_j}" if len_p!=len_j else f"ok{len_p}!={len_j}"
-
-def size_sap_ruby(carat_1):
-    '''функция для определения размерной группы сапфира и рубина в зависимоти кабашон или фацет огранка
-   , carat_1'''   
-    return df_sizes_sap_ruby[(df_sizes_sap_ruby.low_grade<=carat_1)].sort_values('low_grade').iloc[-1,0]
-
-
-def check_vstlistsapryby(l):
-    '''функция для проверки соответствия словарям'''                           
-    #проверка на пусто или []
-    if l is np.nan or len(l)==0:
-        return np.nan
-    #создать внутренний dfx
-    dfx = pd.DataFrame({'vstlist':l})
     
+    def check_vstlistsapryby(l):
+        '''функция для вывода стандартного листа и отчет проверки соответствия словарям по flagreport=True'''                           
+        #проверка на пусто или []
+        if l is np.nan or len(l)==0:
+            return 'нет распознанных'
+        #создать внутренний dfx
+        dfx = pd.DataFrame({'vstlist':l})
+        cols = ["PCS","GEM","CARAT", "FORM", "CQ"]
+        for c in range(len(cols)):
+            dfx[cols[c]] = dfx.vstlist.apply(lambda x: x[c].lower().strip())
+        dfx['C'] = dfx['CQ'].apply(lambda x: x.split('/')[0].lower().strip())
+        dfx['Q'] = dfx['CQ'].apply(lambda x: x.split('/')[1].lower().strip().split('*')[0].strip())
+        dfx['C_add'] = dfx['CQ'].apply(lambda x: x.split('/')[1].lower().strip().split('*')[-1].strip()\
+                                      if x.__contains__('*') else np.nan)
+
+        #пустить проверки по словарям  dcla_sap, dcol_sap, dgem
+        report = ''
+        report += 'C:ok;' if dfx['C'].apply(lambda x: x in mds.dcol_sap).all() \
+                          else f"C:error:{' '.join(dfx.loc[~dfx['C'].apply(lambda x: x in mds.dcol_sap), 'C'].to_list())};"
+        report+='Q:ok;' if dfx['Q'].apply(lambda x: x in mds.dcla_sap).all()\
+                          else f"Q:error:{' '.join(dfx.loc[~dfx['Q'].apply(lambda x: x in mds.dcla_sap), 'Q'].to_list())};"
+        report+='PCS:ok;' if dfx['PCS'].apply(lambda x: x!='').all()\
+                          else 'PCS:error:'';'
+        report += 'GEM:ok;' if dfx['GEM'].apply(lambda x: x in mds.dgem).all() \
+                          else f"GEM:error:{' '.join(dfx.loc[~dfx['GEM'].apply(lambda x: x in mds.dgem), 'GEM'].to_list())};"
+        return report 
+
+    def sap_list2std(l):
+        '''функция для вывода стандартного листа и отчет проверки соответствия словарям по flagreport=True'''                           
+        #проверка на пусто или []
+        if l is np.nan or len(l)==0:
+            return [],'нет распознанных'
+        #создать внутренний dfx
+        dfx = pd.DataFrame({'vstlist':l})
+        cols = ["PCS","GEM","CARAT", "FORM", "CQ"]
+        for c in range(len(cols)):
+            dfx[cols[c]] = dfx.vstlist.apply(lambda x: x[c].lower().strip())
+        dfx['C'] = dfx['CQ'].apply(lambda x: x.split('/')[0].lower().strip())
+        dfx['Q'] = dfx['CQ'].apply(lambda x: x.split('/')[1].lower().strip().split('*')[0].strip())
+        dfx['C_add'] = dfx['CQ'].apply(lambda x: x.split('/')[1].lower().strip().split('*')[-1].strip()\
+                                      if x.__contains__('*') else np.nan)
+
+        #пустить проверки по словарям  dcla_sap, dcol_sap, dgem
+        report = ''
+        report += 'C:ok;' if dfx['C'].apply(lambda x: x in mds.dcol_sap).all() \
+                          else f"C:error:{' '.join(dfx.loc[~dfx['C'].apply(lambda x: x in mds.dcol_sap), 'C'].to_list())};"
+        report+='Q:ok;' if dfx['Q'].apply(lambda x: x in mds.dcla_sap).all()\
+                          else f"Q:error:{' '.join(dfx.loc[~dfx['Q'].apply(lambda x: x in mds.dcla_sap), 'Q'].to_list())};"
+        report+='PCS:ok;' if dfx['PCS'].apply(lambda x: x!='').all()\
+                          else 'PCS:error:'';'
+        report += 'GEM:ok;' if dfx['GEM'].apply(lambda x: x in mds.dgem).all() \
+                          else f"GEM:error:{' '.join(dfx.loc[~dfx['GEM'].apply(lambda x: x in mds.dgem), 'GEM'].to_list())};"
+        dfx['GEM_s'] = dfx['GEM'].map(mds.dgem)
+        dfx['C_s'] = dfx['C'].map(mds.dcol_sap)
+        dfx['Q_s'] = dfx['Q'].map(mds.dcla_sap)
+        std_list = dfx[['PCS', 'GEM_s', 'CARAT', 'FORM', 'C_s', 'Q_s', 'C_add']].to_numpy().tolist()
+        return std_list, report 
+
+    report = error_by_lens_patts(t)
+    rawlist = vst2listsap_notna(t)
+    #готовим вывот стандартного листа вставок
+    res = sap_list2std(rawlist)
+    report += ';'+res[1]
+    std_list = res[0]
     
-    cols = ["PCS","GEM","CARAT", "FORM", "CQ"]
-    for c in range(len(cols)):
-        dfx[cols[c]] = dfx.vstlist.apply(lambda x: x[c].lower().strip())
-    dfx['C'] = dfx['CQ'].apply(lambda x: x.split('/')[0].lower().strip())
-    dfx['Q'] = dfx['CQ'].apply(lambda x: x.split('/')[1].lower().strip().split('*')[0].strip())
-    dfx['C_add'] = dfx['CQ'].apply(lambda x: x.split('/')[1].lower().strip().split('*')[-1].strip()\
-                                  if x.__contains__('*') else np.nan)
+      
+    return res[0],res[1]
     
-    
-  
-    #пустить проверки по словарям  dcla_sap, dcol_sap
-    report = ''
-    report += 'C:ok;' if dfx['C'].apply(lambda x: x in dcol_sap).all() \
-                      else f"C:error;"
-    report+='Q:ok;' if dfx['Q'].apply(lambda x: x in dcla_sap).all()\
-                      else 'Q:error;'
-    report+='PCS:ok;' if dfx['PCS'].apply(lambda x: x!='').all()\
-                      else 'PCS:error;'
-    #если есть хоть одна ошибка не считать! 
-    return report
+
+
+
+
+
+
 '''    
     try:
-        if report.__contains__('error'):
+        if report.__contains__('error'):g
             raise ValueError("имеется ошибка")
         dfx['PCS'] = dfx['PCS'].astype('int')
         dfx['CARAT'] = dfx['CARAT'].str.replace(',','.').astype('float')
